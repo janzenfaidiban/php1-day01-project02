25d2e15736d468ab7bbff9c6acb9fa4f142672a8
Janzen Faidiban <janzenfaidiban@gmail.com>
2022-05-19 19:59:08 +0900

    final project02

---
+++ index02.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP 1 Day 01 Project 02</title>
+</head>
+<body>
+
+<?php 
+    echo "Project 02";
+    echo "<br>";
+    print "This is using Print";
+?>
+    
+</body>
+</html>
